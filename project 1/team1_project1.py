8fc7478076e957292e553e074481c0744d17aa4d
chaseleif <chaseleif@icloud.com>
2019-02-11 19:24:26 -0600

    Project near complete
    -Added a comment header
    -Put the Decompiler method in a class which is ran from main()
    -Added file output, ensure output filename is correct format
    -Changed all print statements to instead 'print' to the file
    -Verified 11 bit opcode range correct for all commands required
    -Fixed the IM format opcodes (should verify I am getting the shamt correctly though)
    -Formatted output more, test run matches expected output file exactly

--- project 1/team1_project1.py
+++ project 1/team1_project1.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python
+#  team1_project1.py
+#  Chase Phelps and Trenton Hohle
+#  this converts some machine code into LEGv8 asm
+#  input file is given as a command line argument
+#  output file is (infilename) + out_dis.txt
 
 import sys
 
@@ -21,153 +26,166 @@ def twoscbinarytexttodecimal(binarytext):
     return numba
 
 
-def main():
-    fullmachinecode = [line.rstrip() for line in open(sys.argv[1], 'rb')]
+class Decompiler:
 
-    opcodes = []
-    textend = 0
-    instructionformat = []
-    theinstruction = []
-    for i in range(len(fullmachinecode)):
-        opcodes.append(binarytexttodecimal(fullmachinecode[i][0:11]))
-        theinstruction.append('')
-#  text/data break
-        if opcodes[i] == 2038:
-            instructionformat.append('BR')
-            textend = i
-            break
-# B - format
-        if 160 <= opcodes[i] <= 191:
-            instructionformat.append('B')
-# R - format
-        elif (opcodes[i] == 1112 or opcodes[i] == 1624 or
-              opcodes[i] == 1691 or opcodes[i] == 1690 or
-              opcodes[i] == 1104 or opcodes[i] == 1360 or opcodes[i] == 1616):
-            instructionformat.append('R')
-            if opcodes[i] == 1112:
-                theinstruction[i] = 'ADD'
-            elif opcodes[i] == 1624:
-                theinstruction[i] = 'SUB'
-            elif opcodes[i] == 1691:
-                theinstruction[i] = 'LSL'
-            elif opcodes[i] == 1690:
-                theinstruction[i] = 'LSR'
-            elif opcodes[i] == 1104:
-                theinstruction[i] = 'AND'
-            elif opcodes[i] == 1360:
-                theinstruction[i] = 'ORR'
-            elif opcodes[i] == 1616:
-                theinstruction[i] = 'EOR'
-# I - format
-        elif (opcodes[i] == 1160 or opcodes[i] == 1161 or
-              opcodes[i] == 1672 or opcodes[i] == 1673):
-            instructionformat.append('I')
-            if 1160 <= opcodes[i] <= 1161:
-                theinstruction[i] = 'ADDI'
-            elif 1672 <= opcodes[i] <= 1673:
-                theinstruction[i] = 'SUBI'
-# D - format
-        elif opcodes[i] == 1986 or opcodes[i] == 1984:
-            instructionformat.append('D')
-            if opcodes[i] == 1986:
-                theinstruction[i] = 'LDUR'
-            else:
-                theinstruction[i] = 'STUR'
-#  CB - format
-        elif opcodes[i] == 1440 or opcodes[i] == 1448:
-            instructionformat.append('CB')
-            if opcodes[i] == 1440:
-                theinstruction[i] = 'CBZ'
-            else:
-                theinstruction[i] = 'CBNZ'
-# IM - format
-        elif (opcodes[i] == 1684 or opcodes[i] == 1687 or
-              opcodes[i] == 1940 or opcodes[i] == 1943):
-            instructionformat.append('IM')
-            if 1684 <= opcodes[i] <= 1687:
-                theinstruction[i] = 'MOVZ'
-            else:
-                theinstruction[i] = 'MOVK'
-# NOP
-        elif opcodes[i] == 0:
-            instructionformat.append('NOP')
-        else:
-            instructionformat.append('OTHER')
+    def __init__(self):
+        pass
 
-    for i in range(len(fullmachinecode)):
-        if i > textend:
-            print fullmachinecode[i] + "\t\t" + str(i * 4 + 96) + "\t\t",
-            print (str(twoscbinarytexttodecimal(fullmachinecode[i])) if fullmachinecode[i][0] == '1'
-                   else binarytexttodecimal(fullmachinecode[i])),
-            print "\n",
-            continue
-        if instructionformat[i] == 'B':
-            print fullmachinecode[i][0:6] + " " + fullmachinecode[i][6:32] + "   ",
-        elif instructionformat[i] == 'R':
-            print fullmachinecode[i][0:11] + " " + fullmachinecode[i][11:16],
-            print fullmachinecode[i][16:22] + " " + fullmachinecode[i][22:27],
-            print fullmachinecode[i][27:32],
-        elif instructionformat[i] == 'I':
-            print fullmachinecode[i][0:10] + " " + fullmachinecode[i][10:22],
-            print fullmachinecode[i][22:27] + " " + fullmachinecode[i][27:32],
-        elif instructionformat[i] == 'D':
-            print fullmachinecode[i][0:11] + " " + fullmachinecode[i][11:20],
-            print fullmachinecode[i][20:22] + " " + fullmachinecode[i][22:27],
-            print fullmachinecode[i][27:32],
-        elif instructionformat[i] == 'CB':
-            print fullmachinecode[i][0:8] + " " + fullmachinecode[i][8:27],
-            print fullmachinecode[i][27:32] + "    ",
-        elif instructionformat[i] == 'IM':
-            print fullmachinecode[i][0:9] + " " + fullmachinecode[i][9:11],
-            print fullmachinecode[i][11:27] + " " + fullmachinecode[i][27:32],
-        elif instructionformat[i] == 'BR':
-            print fullmachinecode[i][0:8] + " " + fullmachinecode[i][8:11],
-            print fullmachinecode[i][11:16] + " " + fullmachinecode[i][16:21],
-            print fullmachinecode[i][21:26] + " " + fullmachinecode[i][26:32],
-        elif instructionformat[i] == 'NOP':
-            print fullmachinecode[i] + " " + instructionformat[i]
-            continue
-        else:
-            print "something bad\t,"
-        print "\t" + str(i*4+96) + "\t\t",
+    @staticmethod
+    def decompile(infile):
+        fullmachinecode = [line.rstrip() for line in open(infile, 'rb')]
+#  opened input file, now get our output filename
+        textend = infile.find('.')
+        outfilename = infile[:textend] + "out_dis.txt"
+        outfile = open(outfilename, 'w')
 
-        if instructionformat[i] == 'B':
-            print "B\t\t#" + str(binarytexttodecimal(fullmachinecode[i][6:32]))
-        elif instructionformat[i] == 'R':
-            print theinstruction[i] + "\t\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
-            print "R" + str(binarytexttodecimal(fullmachinecode[i][22:27])) + ",",
-            if theinstruction[i] == 'LSL' or theinstruction[i] == 'LSR':
-                if fullmachinecode[i][16] == '0':
-                    print "#" + str(binarytexttodecimal(fullmachinecode[i][16:22]))
+        opcodes = []
+        textend = 0
+        instructionformat = []
+        theinstruction = []
+        for i in range(len(fullmachinecode)):
+            opcodes.append(binarytexttodecimal(fullmachinecode[i][0:11]))
+            theinstruction.append('')
+    #  text/data break
+            if opcodes[i] == 2038:
+                instructionformat.append('BR')
+                textend = i
+                break
+    # B - format
+            if 160 <= opcodes[i] <= 191:
+                instructionformat.append('B')
+    # R - format
+            elif (opcodes[i] == 1112 or opcodes[i] == 1624 or
+                  opcodes[i] == 1691 or opcodes[i] == 1690 or
+                  opcodes[i] == 1104 or opcodes[i] == 1360 or opcodes[i] == 1872):
+                instructionformat.append('R')
+                if opcodes[i] == 1112:
+                    theinstruction[i] = 'ADD'
+                elif opcodes[i] == 1624:
+                    theinstruction[i] = 'SUB'
+                elif opcodes[i] == 1691:
+                    theinstruction[i] = 'LSL'
+                elif opcodes[i] == 1690:
+                    theinstruction[i] = 'LSR'
+                elif opcodes[i] == 1104:
+                    theinstruction[i] = 'AND'
+                elif opcodes[i] == 1360:
+                    theinstruction[i] = 'ORR'
+                elif opcodes[i] == 1872:
+                    theinstruction[i] = 'EOR'
+    # I - format
+            elif (1160 <= opcodes[i] <= 1161 or
+                  1672 <= opcodes[i] <= 1673):
+                instructionformat.append('I')
+                if 1160 <= opcodes[i] <= 1161:
+                    theinstruction[i] = 'ADDI'
+                elif 1672 <= opcodes[i] <= 1673:
+                    theinstruction[i] = 'SUBI'
+    # D - format
+            elif opcodes[i] == 1986 or opcodes[i] == 1984:
+                instructionformat.append('D')
+                if opcodes[i] == 1986:
+                    theinstruction[i] = 'LDUR'
                 else:
-                    print "#" + str(twoscbinarytexttodecimal(fullmachinecode[i][16:22]))
+                    theinstruction[i] = 'STUR'
+    #  CB - format
+            elif (1440 <= opcodes[i] <= 1447 or
+                  1448 <= opcodes[i] <= 1455):
+                instructionformat.append('CB')
+                if 1440 <= opcodes[i] <= 1447:
+                    theinstruction[i] = 'CBZ'
+                else:
+                    theinstruction[i] = 'CBNZ'
+    # IM - format
+            elif (1684 <= opcodes[i] <= 1687 or
+                  1940 <= opcodes[i] <= 1943):
+                instructionformat.append('IM')
+                if 1684 <= opcodes[i] <= 1687:
+                    theinstruction[i] = 'MOVZ'
+                else:
+                    theinstruction[i] = 'MOVK'
+    # NOP
+            elif opcodes[i] == 0:
+                instructionformat.append('NOP')
             else:
-                print "R" + str(binarytexttodecimal(fullmachinecode[i][11:16]))
-        elif instructionformat[i] == 'I':
-            print (theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ", R" +
-                   str(binarytexttodecimal(fullmachinecode[i][22:27])) + ", #" +
-                   str(binarytexttodecimal(fullmachinecode[i][10:22])))
-        elif instructionformat[i] == 'D':
-            print theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
-            print "[R" + str(binarytexttodecimal(fullmachinecode[i][22:27])) + ",",
-            print "#" + str(binarytexttodecimal(fullmachinecode[i][11:20])) + "]"
-        elif instructionformat[i] == 'CB':
-            print theinstruction[i] + " \tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
-            print "#" + str(binarytexttodecimal(fullmachinecode[i][8:27]))
-#  need to find out IM opcode / MOVZ/MOVK details!!! #
-        elif instructionformat[i] == 'IM':
-            print theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
-            print str(binarytexttodecimal(fullmachinecode[i][11:27]))
-        elif instructionformat[i] == 'BR':
-            print "BREAK"
-        elif instructionformat[i] == 'NOP':
-            print "NOP"
-        else:
-            print "something bad\t"
+                instructionformat.append('OTHER')
+
+        for i in range(len(fullmachinecode)):
+            if i > textend:
+                print >> outfile, fullmachinecode[i] + "\t" + str(i * 4 + 96) + "\t",
+                print >> outfile, (str(twoscbinarytexttodecimal(fullmachinecode[i])) if fullmachinecode[i][0] == '1'
+                       else binarytexttodecimal(fullmachinecode[i])),
+                print >> outfile, "\n",
+                continue
+            if instructionformat[i] == 'B':
+                print >> outfile, fullmachinecode[i][0:6] + " " + fullmachinecode[i][6:32] + "   ",
+            elif instructionformat[i] == 'R':
+                print >> outfile, fullmachinecode[i][0:11] + " " + fullmachinecode[i][11:16],
+                print >> outfile, fullmachinecode[i][16:22] + " " + fullmachinecode[i][22:27],
+                print >> outfile, fullmachinecode[i][27:32],
+            elif instructionformat[i] == 'I':
+                print >> outfile, fullmachinecode[i][0:10] + " " + fullmachinecode[i][10:22],
+                print >> outfile, fullmachinecode[i][22:27] + " " + fullmachinecode[i][27:32],
+            elif instructionformat[i] == 'D':
+                print >> outfile, fullmachinecode[i][0:11] + " " + fullmachinecode[i][11:20],
+                print >> outfile, fullmachinecode[i][20:22] + " " + fullmachinecode[i][22:27],
+                print >> outfile, fullmachinecode[i][27:32],
+            elif instructionformat[i] == 'CB':
+                print >> outfile, fullmachinecode[i][0:8] + " " + fullmachinecode[i][8:27],
+                print >> outfile, fullmachinecode[i][27:32] + " ",
+            elif instructionformat[i] == 'IM':
+                print >> outfile, fullmachinecode[i][0:9] + " " + fullmachinecode[i][9:11],
+                print >> outfile, fullmachinecode[i][11:27] + " " + fullmachinecode[i][27:32],
+            elif instructionformat[i] == 'BR':
+                print >> outfile, fullmachinecode[i][0:8] + " " + fullmachinecode[i][8:11],
+                print >> outfile, fullmachinecode[i][11:16] + " " + fullmachinecode[i][16:21],
+                print >> outfile, fullmachinecode[i][21:26] + " " + fullmachinecode[i][26:32],
+            elif instructionformat[i] == 'NOP':
+                print >> outfile, fullmachinecode[i] + " " + instructionformat[i]
+                continue
+            else:
+                print >> outfile, "something bad\t,"
+            print >> outfile, "\t" + str(i*4+96) + "\t",
+
+            if instructionformat[i] == 'B':
+                print >> outfile, "B\t#" + str(binarytexttodecimal(fullmachinecode[i][6:32]))
+            elif instructionformat[i] == 'R':
+                print >> outfile, theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
+                print >> outfile, "R" + str(binarytexttodecimal(fullmachinecode[i][22:27])) + ",",
+                if theinstruction[i] == 'LSL' or theinstruction[i] == 'LSR':
+                    if fullmachinecode[i][16] == '0':
+                        print >> outfile, "#" + str(binarytexttodecimal(fullmachinecode[i][16:22]))
+                    else:
+                        print >> outfile, "#" + str(twoscbinarytexttodecimal(fullmachinecode[i][16:22]))
+                else:
+                    print >> outfile, "R" + str(binarytexttodecimal(fullmachinecode[i][11:16]))
+            elif instructionformat[i] == 'I':
+                print >> outfile, (theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ", R" +
+                       str(binarytexttodecimal(fullmachinecode[i][22:27])) + ","),
+                print >> outfile, ("#" + str(twoscbinarytexttodecimal(fullmachinecode[i][10:22])) if fullmachinecode[i][10] == '1'
+                       else "#" + str(binarytexttodecimal(fullmachinecode[i][10:22])))
+            elif instructionformat[i] == 'D':
+                print >> outfile, theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
+                print >> outfile, "[R" + str(binarytexttodecimal(fullmachinecode[i][22:27])) + ",",
+                print >> outfile, "#" + str(binarytexttodecimal(fullmachinecode[i][11:20])) + "]"
+            elif instructionformat[i] == 'CB':
+                print >> outfile, theinstruction[i] + " \tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
+                print >> outfile, "#" + str(binarytexttodecimal(fullmachinecode[i][8:27]))
+            elif instructionformat[i] == 'IM':
+                print >> outfile, theinstruction[i] + "\tR" + str(binarytexttodecimal(fullmachinecode[i][27:32])) + ",",
+                print >> outfile, str(binarytexttodecimal(fullmachinecode[i][11:27])) + ",",
+                print >> outfile, "LSL " + str(16*binarytexttodecimal(fullmachinecode[i][9:11]))
+            elif instructionformat[i] == 'BR':
+                print >> outfile, "BREAK"
+            elif instructionformat[i] == 'NOP':
+                print >> outfile, "NOP"
+            else:
+                print >> outfile, "something bad\t"
+        outfile.close()
 
 
 #  function called when script ran from command line
 #  uses first arg, sys.argv[1], as input filepath
 #  opens that file and 'decompiles' machine code to asm
 if __name__ == "__main__":
-    main()
+    Decompiler.decompile(sys.argv[1])
