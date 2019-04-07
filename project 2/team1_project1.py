75539cd8793d70f63037744bccaffee61e56b966
chaseleif <chaseleif@icloud.com>
2019-04-07 01:48:53 -0500

    project 2 pretty much done.
    *Fixed spacing, fixed a couple other issues
    *Need to change the 'debug' output to print to file instead of screen
    *Test the LSR? I think I figured out a method that works
    *EOR does not work when mixing pos/neg numbers, as SHOULD BE EXPECTED with Python

--- project 2/team1_project1.py
+++ project 2/team1_project1.py
@@ -171,43 +171,47 @@ class Decompiler:
                 outline += self.fullmachinecode[i] + "\t" + str(memaddr) + "\tNOP"
 # R - register
             elif self.instructionformat[i] == 'R':
-                outline += self.fullmachinecode[i][0:11] + " " + self.fullmachinecode[i][11:16] + " " + self.fullmachinecode[i][16:22]
-                outline += " " + self.fullmachinecode[i][22:27] + " " + self.fullmachinecode[i][27:32] + "\t" + str(memaddr)
-                outline += "\t" + str(self.instructions[i]) + "\tR" + str(self.rdarray[i]) + ", R" + str(self.rnarray[i]) + ", "
+                outline += self.fullmachinecode[i][0:11] + " " + self.fullmachinecode[i][11:16] + " "
+                outline += self.fullmachinecode[i][16:22] + " " + self.fullmachinecode[i][22:27] + " "
+                outline += self.fullmachinecode[i][27:32] + "\t" + str(memaddr) + "\t" + str(self.instructions[i])
+                outline += "\tR" + str(self.rdarray[i]) + ", R" + str(self.rnarray[i]) + ", "
                 if self.instructions[i] == 'LSL' or self.instructions[i] == 'LSR' or self.instructions[i] == 'ASR':
                     outline += "#" + str(self.shamtarray[i])
                 else:
                     outline += "R" + str(self.rmarray[i])
 # I - immediate
             elif self.instructionformat[i] == 'I':
-                outline += self.fullmachinecode[i][0:10] + " " + self.fullmachinecode[i][10:22] + " " + self.fullmachinecode[i][22:27]
-                outline += " " + self.fullmachinecode[i][27:32] + "\t" + str(memaddr) + "\t" + self.instructions[i] + "\tR"
-                outline += str(self.rdarray[i]) + ", R" + str(self.rnarray[i]) + ", #" + str(self.immarray[i])
+                outline += self.fullmachinecode[i][0:10] + " " + self.fullmachinecode[i][10:22] + " "
+                outline += self.fullmachinecode[i][22:27] + " " + self.fullmachinecode[i][27:32] + "\t" + str(memaddr)
+                outline += "\t" + self.instructions[i] + "\tR" + str(self.rdarray[i]) + ", R" + str(self.rnarray[i])
+                outline += ", #" + str(self.immarray[i])
 # D - data load / store
             elif self.instructionformat[i] == 'D':
-                outline += self.fullmachinecode[i][0:11] + " " + self.fullmachinecode[i][11:20] + " " + self.fullmachinecode[i][20:22]
-                outline += " " + self.fullmachinecode[i][22:27] + " " + self.fullmachinecode[i][27:32] + "\t" + str(memaddr)
-                outline += "\t" + self.instructions[i] + "\tR" + str(self.rdarray[i]) + ", [R" + str(self.rnarray[i]) + ", #"
-                outline += str(self.addrarray[i]) + "]"
+                outline += self.fullmachinecode[i][0:11] + " " + self.fullmachinecode[i][11:20] + " "
+                outline += self.fullmachinecode[i][20:22] + " " + self.fullmachinecode[i][22:27] + " "
+                outline += self.fullmachinecode[i][27:32] + "\t" + str(memaddr) + "\t" + self.instructions[i] + "\tR"
+                outline += str(self.rdarray[i]) + ", [R" + str(self.rnarray[i]) + ", #" + str(self.addrarray[i]) + "]"
 # IM - move immediate / multiple
             elif self.instructionformat[i] == 'IM':
-                outline += self.fullmachinecode[i][0:9] + " " + self.fullmachinecode[i][9:11] + " " + self.fullmachinecode[i][11:27]
-                outline += " " + self.fullmachinecode[i][27:32] + "\t" + str(memaddr) + "\t" + self.instructions[i] + "\tR"
-                outline += str(self.rdarray[i]) + ", " + str(self.immarray[i]) + ", LSL " + str(self.shamtarray[i])
+                outline += self.fullmachinecode[i][0:9] + " " + self.fullmachinecode[i][9:11] + " "
+                outline += self.fullmachinecode[i][11:27] + " " + self.fullmachinecode[i][27:32] + "\t" + str(memaddr)
+                outline += "\t" + self.instructions[i] + "\tR" + str(self.rdarray[i]) + ", " + str(self.immarray[i])
+                outline += ", LSL " + str(self.shamtarray[i])
 # B - this is the branch instruction
             elif self.instructionformat[i] == 'B':
                 outline += self.fullmachinecode[i][0:6] + " " + self.fullmachinecode[i][6:32] + "\t"
                 outline += str(memaddr) + "\tB\t#" + str(self.addrarray[i])
 # CB - conditional branch
             elif self.instructionformat[i] == 'CB':
-                outline += self.fullmachinecode[i][0:8] + " " + self.fullmachinecode[i][8:27] + " " + self.fullmachinecode[i][27:32]
-                outline += "\t" + str(memaddr) + "\t" + self.instructions[i] + "\tR" + str(self.rdarray[i]) + ", #"
-                outline += str(self.offsarray[i])
+                outline += self.fullmachinecode[i][0:8] + " " + self.fullmachinecode[i][8:27] + " "
+                outline += self.fullmachinecode[i][27:32] + "\t" + str(memaddr) + "\t" + self.instructions[i] + "\tR"
+                outline += str(self.rdarray[i]) + ", #" + str(self.offsarray[i])
 # BR - this is the data segment break
             elif self.instructionformat[i] == 'BR':
-                outline += self.fullmachinecode[i][0:8] + " " + self.fullmachinecode[i][8:11] + " " + self.fullmachinecode[i][11:16]
-                outline += " " + self.fullmachinecode[i][16:21] + " " + self.fullmachinecode[i][21:26] + " "
-                outline += self.fullmachinecode[i][26:32] + "\t" + str(memaddr) + "\tBREAK"
+                outline += self.fullmachinecode[i][0:8] + " " + self.fullmachinecode[i][8:11] + " "
+                outline += self.fullmachinecode[i][11:16] + " " + self.fullmachinecode[i][16:21] + " "
+                outline += self.fullmachinecode[i][21:26] + " " + self.fullmachinecode[i][26:32] + "\t" + str(memaddr)
+                outline += "\tBREAK"
 # write output line and restart loop if more lines to process
             print >> outfile, outline
 # for i in self.fullmachinecode[] end loop
@@ -219,34 +223,36 @@ class Decompiler:
 
         datasegment = {}
         dataendindex = -1
-        for i in range(self.datastartindex+1,len(self.fullmachinecode)):
+        for i in range(self.datastartindex + 1, len(self.fullmachinecode)):
             datasegment[(i*4)+96] = self.fullmachinecode[i]
             dataendindex = (i*4)+96
 
         cyclecounter = 0
-        pccounter = -1
+        pccounter = 0
         registers = [0] * 32
         while self.instructionformat[pccounter] != 'BR':
-            pccounter += 1
             cyclecounter += 1
 
             outline = "cycle: " + str(cyclecounter) + "\t" + str((pccounter * 4) + 96) + "\t"
             if self.instructionformat[pccounter] == 'NOP':
                 outline += "NOP"
             elif self.instructionformat[pccounter] == 'R':
-                outline += str(self.instructions[pccounter]) + "\tR" + str(self.rdarray[pccounter]) + ", R" + str(self.rnarray[pccounter]) + ", "
-                if self.instructions[pccounter] == 'LSL' or self.instructions[pccounter] == 'LSR' or self.instructions[pccounter] == 'ASR':
+                outline += str(self.instructions[pccounter]) + "\tR" + str(self.rdarray[pccounter])
+                outline += ", R" + str(self.rnarray[pccounter]) + ", "
+                if self.instructions[pccounter] == 'LSL' or self.instructions[pccounter] == 'LSR'\
+                        or self.instructions[pccounter] == 'ASR':
                     outline += "#" + str(self.shamtarray[pccounter])
                 else:
                     outline += "R" + str(self.rmarray[pccounter])
             elif self.instructionformat[pccounter] == 'I':
-                outline += self.instructions[pccounter] + "\tR" + str(self.rdarray[pccounter]) + ", R" + str(self.rnarray[pccounter]) + ", #" + str(self.immarray[pccounter])
+                outline += self.instructions[pccounter] + "\tR" + str(self.rdarray[pccounter])
+                outline += ", R" + str(self.rnarray[pccounter]) + ", #" + str(self.immarray[pccounter])
             elif self.instructionformat[pccounter] == 'D':
-                outline += self.instructions[pccounter] + "\tR" + str(self.rdarray[pccounter]) + ", [R" + str(self.rnarray[pccounter]) + ", #"
-                outline += str(self.addrarray[pccounter]) + "]"
+                outline += self.instructions[pccounter] + "\tR" + str(self.rdarray[pccounter])
+                outline += ", [R" + str(self.rnarray[pccounter]) + ", #" + str(self.addrarray[pccounter]) + "]"
             elif self.instructionformat[pccounter] == 'IM':
-                outline += self.instructions[pccounter] + "\tR"
-                outline += str(self.rdarray[pccounter]) + ", " + str(self.immarray[pccounter]) + ", LSL " + str(self.shamtarray[pccounter])
+                outline += self.instructions[pccounter] + "\tR" + str(self.rdarray[pccounter]) + ", "
+                outline += str(self.immarray[pccounter]) + ", LSL " + str(self.shamtarray[pccounter])
             elif self.instructionformat[pccounter] == 'B':
                 outline += "B\t#" + str(self.addrarray[pccounter])
             elif self.instructionformat[pccounter] == 'CB':
@@ -254,23 +260,31 @@ class Decompiler:
                 outline += str(self.offsarray[pccounter])
             elif self.instructionformat[pccounter] == 'BR':
                 outline += "BREAK"
+# flag for whether to update pc counter +1 or if branch is changing it
+            takebranch = 0
 # R -> instructions[] rdarray[] rnarray[] { rmarray[] or shamtarray[] }
             if self.instructions[pccounter] == 'ADD':
-                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] + registers[self.rmarray[pccounter]]
+                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]]\
+                                                     + registers[self.rmarray[pccounter]]
             elif self.instructions[pccounter] == 'SUB':
-                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] - registers[self.rmarray[pccounter]]
+                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]]\
+                                                     - registers[self.rmarray[pccounter]]
             elif self.instructions[pccounter] == 'AND':
-                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] & registers[self.rmarray[pccounter]]
+                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]]\
+                                                     & registers[self.rmarray[pccounter]]
             elif self.instructions[pccounter] == 'ORR':
-                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] | registers[self.rmarray[pccounter]]
+                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]]\
+                                                     | registers[self.rmarray[pccounter]]
             elif self.instructions[pccounter] == 'EOR':
-                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] ^ registers[self.rmarray[pccounter]]
+                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]]\
+                                                     ^ registers[self.rmarray[pccounter]]
             elif self.instructions[pccounter] == 'LSL':
                 registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] << self.shamtarray[pccounter]
             elif self.instructions[pccounter] == 'LSR':
                 registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] >> self.shamtarray[pccounter]
             elif self.instructions[pccounter] == 'ASR':
-                registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] >> self.shamtarray[pccounter]
+                registers[self.rdarray[pccounter]] = 0xFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFFF >> self.shamtarray[pccounter]
+                registers[self.rdarray[pccounter]] += registers[self.rnarray[pccounter]] >> self.shamtarray[pccounter]
 # I-> instructions[] rdarray[] rnarray[] immarray[]
             elif self.instructions[pccounter] == 'ADDI':
                 registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] + self.immarray[pccounter]
@@ -278,13 +292,13 @@ class Decompiler:
                 registers[self.rdarray[pccounter]] = registers[self.rnarray[pccounter]] - self.immarray[pccounter]
 # D-> instructions[] rdarray[] rnarray[] addrarray[]
             elif self.instructions[pccounter] == 'LDUR':
-                jumpaddress = self.rnarray[pccounter] + self.addrarray[pccounter]
+                jumpaddress = registers[self.rnarray[pccounter]] + self.addrarray[pccounter]
                 if jumpaddress in datasegment:
                     registers[self.rdarray[pccounter]] = datasegment[jumpaddress]
                 else:
                     registers[self.rdarray[pccounter]] = 0
             elif self.instructions[pccounter] == 'STUR':
-                jumpaddress = self.rnarray[pccounter] + self.addrarray[pccounter]
+                jumpaddress = registers[self.rnarray[pccounter]] + self.addrarray[pccounter]
                 datasegment[jumpaddress] = registers[self.rdarray[pccounter]]
                 if jumpaddress > dataendindex:
                     dataendindex = jumpaddress
@@ -293,24 +307,32 @@ class Decompiler:
                 registers[self.rdarray[pccounter]] = self.immarray[pccounter] << self.shamtarray[pccounter]
             elif self.instructions[pccounter] == 'MOVK':
                 keepvalue = 0
+                print "The shamt is " + str(self.shamtarray[pccounter])
                 if self.shamtarray[pccounter] == 0:
-                    keepvalue = self.rdarray[pccounter] & 0xFFFFFFFFFFFF0000
+                    keepvalue = registers[self.rdarray[pccounter]] & 0xFFFFFFFFFFFF0000
                 elif self.shamtarray[pccounter] == 16:
-                    keepvalue = self.rdarray[pccounter] & 0xFFFFFFFF0000FFFF
+                    keepvalue = registers[self.rdarray[pccounter]] & 0xFFFFFFFF0000FFFF
                 elif self.shamtarray[pccounter] == 32:
-                    keepvalue = self.rdarray[pccounter] & 0xFFFF0000FFFFFFFF
+                    keepvalue = registers[self.rdarray[pccounter]] & 0xFFFF0000FFFFFFFF
                 elif self.shamtarray[pccounter] == 48:
-                    keepvalue = self.rdarray[pccounter] & 0x0000FFFFFFFFFFFF
+                    keepvalue = registers[self.rdarray[pccounter]] & 0x0000FFFFFFFFFFFF
+                print "The keep is " + str(keepvalue)
                 keepvalue += self.immarray[pccounter] << self.shamtarray[pccounter]
+                print "The keep is now " + str(keepvalue)
                 registers[self.rdarray[pccounter]] = keepvalue
 # B-> addrarray[]
-#            elif self.instructions[pccounter] == 'B':
-#                pccounter = (self.addrarray[pccounter] / 4) - 97
+            elif self.instructions[pccounter] == 'B':
+                pccounter += self.addrarray[pccounter]
+                takebranch = 1
 #CB-> rdarray[] offsarray[]
-            elif self.instructions[pccounter] == 'CBZ' and self.rdarray[pccounter] == 0:
+            elif self.instructions[pccounter] == 'CBZ' and registers[self.rdarray[pccounter]] == 0:
                 pccounter += self.offsarray[pccounter]
-            elif self.instructions[pccounter] == 'CBNZ' and self.rdarray[pccounter] != 0:
+                takebranch = 1
+            elif self.instructions[pccounter] == 'CBNZ' and registers[self.rdarray[pccounter]] != 0:
                 pccounter += self.offsarray[pccounter]
+                takebranch = 1
+            if not takebranch:
+                pccounter += 1
             print "===================="
             outline += "\n\nregisters:\nr00:\t" + str(registers[0]) + "\t" + str(registers[1]) + "\t"
             outline += str(registers[2]) + "\t" + str(registers[3]) + "\t" + str(registers[4]) + "\t"
@@ -325,11 +347,12 @@ class Decompiler:
             outline += str(registers[26]) + "\t" + str(registers[27]) + "\t" + str(registers[28]) + "\t"
             outline += str(registers[29]) + "\t" + str(registers[30]) + "\t" + str(registers[31]) + "\n\n"
             if dataendindex > 0:
+                outline += "data:\n"
                 datacounter = ((self.datastartindex+1) * 4) + 96
                 while datacounter <= dataendindex:
                     outline += str(datacounter) + ": "
                     donewline = 0
-                    while donewline <=32:
+                    while donewline <= 32:
                         if (datacounter+donewline) in datasegment:
                             outline += str(datasegment[datacounter+donewline]) + "\t"
                         else:
